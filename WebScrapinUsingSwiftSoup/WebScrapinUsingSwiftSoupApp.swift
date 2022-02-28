9bab713ca20bef4c53b951102004307a7de80f59
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-02-28 21:22:06 +0200

    Initial Commit

---
+++ WebScrapinUsingSwiftSoup/WebScrapinUsingSwiftSoupApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebScrapinUsingSwiftSoupApp.swift
+//  WebScrapinUsingSwiftSoup
+//
+//  Created by ramil on 28.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct WebScrapinUsingSwiftSoupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
